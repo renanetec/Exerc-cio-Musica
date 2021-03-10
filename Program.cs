7215686af90cb7955218a9fd141cfa1a22e6e816
renanetec <renanmenezescarlos21@gmail.com>
2021-03-10 19:23:50 -0300

    altera os dados e adiciona a letra da musicá

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exerc_cio_Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;  
+            Console.WriteLine("Impressionando os anjos - Gustavo mioto");
+            Console.WriteLine();
+            Console.WriteLine("=====================================");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.WriteLine("Hoje foi tudo bem, só um pouco cansativo\n Dia duro no trabalho que acabou comigo\n Tô aqui com os pés pra cima pronto pra dormir");                                                                                                                                              
+            Console.WriteLine();
+            Console.WriteLine("A saudade de você é visita frequente\n Que nem a sua tia chata que irritava a gente Ah, saudade da gente");
+            Console.WriteLine();
+            Console.WriteLine("Tirando isso, do resto até que eu tô dando conta\n A Julinha tá banguela e o Pedro só apronta\n O que faltava do carro, eu já quitei a conta");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ah, falando nisso, terminei o livro que você pediu pra eu ler\n E só na página 70, entendi você\n Naquela parte onde diz que o amor é fogo que arde sem se ver");
+            Console.WriteLine();
+            Console.WriteLine("Como é que tá aí?\n De você faz tempo que não ouço nada\n Fala um pouco, sua voz tá tão calada\n Sei que agora deve estar impressionando os anjos com sua risada");
+            Console.WriteLine();
+            Console.WriteLine("Mas de você faz tempo que não ouço nada\n Fala um pouco, sua voz tá tão calada\n Aí de cima, fala alto que eu preciso ouvir\n Como é que tá aí?");
+            Console.WriteLine();
+            Console.ResetColor();
+           
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Exerccio-Musica
+# Exercício-Musica
 Faça um programa que exiba uma letra de música a sua escolha. Utilize cores em destaque no título e nos refrões.
 
 **Nome:** Renan de Oliveira Ferreira menezes
